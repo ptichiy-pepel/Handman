9df8dcf21ae4e7d38a5edbafd34f4297bf4c33d9
Ptichiy Pepel <kililkin@icloud.com>
2022-03-03 23:14:47 +0300

    Теперь handman 1. Может принимать несколько слов. 2. Раскрвает все индексы загаданного символа

--- Handman.py
+++ Handman.py
@@ -2,7 +2,7 @@ import random
 
 
 def hangman():
-    word = input('Загадай слово\n')
+    word = input('Загадай слово или фразу\n')
     wrong_guesses = 0
     stages = ["", 
                 "________      ", 
@@ -14,16 +14,18 @@ def hangman():
     remaining_letters = list(word)
     letter_board = ["__"] * len(word)
     win = False
+    
     print('Добро пожаловать на казнь')
     while wrong_guesses < len(stages) - 1:
         print('\n')
         guess = input("Введите букву: ")
-        if guess in remaining_letters:
-            character_index = remaining_letters.index(guess)
-            letter_board[character_index] = guess
-            remaining_letters[character_index] = '$'
-        else:
+        if guess not in remaining_letters:
             wrong_guesses += 1
+        for i in remaining_letters:
+            if guess in remaining_letters:
+                character_index = remaining_letters.index(guess)
+                letter_board[character_index] = guess
+                remaining_letters[character_index] = '$'
         print((' '.join(letter_board)))
         print('\n'.join(stages[0: wrong_guesses + 1]))
         if '__' not in letter_board:
@@ -33,6 +35,6 @@ def hangman():
             break
     if not win:
         print('\n'.join(stages[0: wrong_guesses]))
-        print('Вы проиграли! Было загадано слово: {}'.format(word))
+        print(f'Вы проиграли! Было загадано слово: {word}')
 
 hangman()
